586d4996ba692e679d121d667d814db1e9aa0e8f
Andy Caldwell <andrew.caldwell@metaswitch.com>
2013-11-29 01:11:18 +0000

    Initialize cURL in test code since mock replicator calls some cURL functions

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <curl/curl.h>
 
 int main(int argc, char **argv) {
+  curl_global_init(CURL_GLOBAL_DEFAULT);
   ::testing::InitGoogleMock(&argc, argv);
   return RUN_ALL_TESTS();
 }
