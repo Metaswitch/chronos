5063b76c66ee8e41d4a1fe0e70779fbe41327848
Andy Caldwell <andrew.caldwell@metaswitch.com>
2014-03-05 00:45:11 +0000

    Asynchronous replication handling

--- src/include/replicator.h
+++ src/include/replicator.h
@@ -4,6 +4,7 @@
 #include <curl/curl.h>
 
 #include "timer.h"
+#include "eventq.h"
 
 // This class is used to replicate timers to the specified replicas, using cURL
 // to handle the HTTP construction and sending.
@@ -13,10 +14,17 @@ public:
   Replicator();
   virtual ~Replicator();
 
+  void run();
   virtual void replicate(Timer*);
 
+  static void* worker_thread_entry_point(void*);
+
 private:
-  CURL* _curl;
+  CURL* create_curl_handle(const std::string& url,
+                           const std::string& body);
+
+  eventq<CURL*> _q;
+  pthread_t _worker_thread;
 };
 
 #endif
