032dcab3b1b50c0c542aac7a6d0c17dfe5b0d4e6
David Collins <david.collins@metaswitch.com>
2014-09-15 17:50:29 -0500

    add dcea def for chronos

---
+++ src/include/chronosdcea.h
@@ -0,0 +1,50 @@
+/**
+ * @file chronosdcea.h  Chronos Craft Log declarations.
+ *
+ * Project Clearwater - IMS in the Cloud
+ * Copyright (C) 2013  Metaswitch Networks Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version, along with the "Special Exception" for use of
+ * the program along with SSL, set forth below. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * The author can be reached by email at clearwater@metaswitch.com or by
+ * post at Metaswitch Networks Ltd, 100 Church St, Enfield EN2 6BQ, UK
+ *
+ * Special Exception
+ * Metaswitch Networks Ltd  grants you permission to copy, modify,
+ * propagate, and distribute a work formed by combining OpenSSL with The
+ * Software, or a work derivative of such a combination, even if such
+ * copying, modification, propagation, or distribution would otherwise
+ * violate the terms of the GPL. You must comply with the GPL in all
+ * respects for all of the code used other than OpenSSL.
+ * "OpenSSL" means OpenSSL toolkit software distributed by the OpenSSL
+ * Project and licensed under the OpenSSL Licenses, or a work based on such
+ * software and licensed under the OpenSSL Licenses.
+ * "OpenSSL Licenses" means the OpenSSL License and Original SSLeay License
+ * under which the OpenSSL Project distributes the OpenSSL toolkit software,
+ * as those licenses appear in the file LICENSE-OPENSSL.
+ */
+
+#ifndef _CHRONOSDCEA_H__
+#define _CHRONOSDCEA_H__
+
+#include "craft_dcea.h"
+
+extern SysLog1<const char*> CL_CHRONOS_CRASHED;
+extern SysLog CL_CHRONOS_STARTED;
+extern SysLog CL_CHRONOS_REACTOR_FAIL;
+extern SysLog CL_CHRONOS_FAIL_CREATE_HTTP_SERVICE;
+extern SysLog CL_CHRONOS_HTTP_SERVICE_AVAILABLE;
+extern SysLog CL_CHRONOS_ENDED;
+extern SysLog1<const char*> CL_CHRONOS_NO_SYSTEM_TIME;
+
+#endif
