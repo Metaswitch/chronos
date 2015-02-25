88c755d313bee984e7fe4208b73e28788781c4ac
Eleanor Merry <eleanor.merry@metaswitch.com>
2015-02-25 12:42:43 +0000

    Remove 0.0.0.0

--- src/main/globals.cpp
+++ src/main/globals.cpp
@@ -4,7 +4,7 @@
 
 #include <fstream>
 
-// Shorten the imported namespace for ease of use.  Notice we don't do this in the 
+// Shorten the imported namespace for ease of use.  Notice we don't do this in the
 // header file to avoid infecting other compilation units' namespaces.
 namespace po = boost::program_options;
 
@@ -15,10 +15,10 @@ Globals* __globals;
 Globals::Globals()
 {
   pthread_rwlock_init(&_lock, NULL);
-  
+
   // Describe the configuration file format.
   _desc.add_options()
-    ("http.bind-address", po::value<std::string>()->default_value("0.0.0.0"), "Address to bind the HTTP server to")
+    ("http.bind-address", po::value<std::string>()->default_value("localhost"), "Address to bind the HTTP server to")
     ("http.bind-port", po::value<int>()->default_value(7253), "Port to bind the HTTP server to")
     ("cluster.localhost", po::value<std::string>()->default_value("localhost"), "The address of the local host")
     ("cluster.node", po::value<std::vector<std::string>>()->multitoken()->default_value(std::vector<std::string>(1, "localhost"), "HOST"), "The addresses of a node in the cluster")
@@ -68,14 +68,14 @@ void Globals::update_config()
   std::string cluster_local_address = conf_map["cluster.localhost"].as<std::string>();
   set_cluster_local_ip(cluster_local_address);
   LOG_STATUS("Cluster local address: %s", cluster_local_address.c_str());
-  
+
   std::vector<std::string> cluster_addresses = conf_map["cluster.node"].as<std::vector<std::string>>();
   set_cluster_addresses(cluster_addresses);
   std::map<std::string, uint64_t> cluster_hashes;
   LOG_STATUS("Cluster nodes:");
 
-  for (std::vector<std::string>::iterator it = cluster_addresses.begin(); 
-                                          it != cluster_addresses.end(); 
+  for (std::vector<std::string>::iterator it = cluster_addresses.begin();
+                                          it != cluster_addresses.end();
                                           ++it)
   {
     LOG_STATUS(" - %s", it->c_str());
@@ -97,7 +97,7 @@ void Globals::update_config()
 // Generates the pre-calculated bloom filter for the given string.
 //
 // Create 3 128-bit hashes, modulo each half down to 0..63 and set those
-// bits in the returned value.  In general this will set ~6 bits in the 
+// bits in the returned value.  In general this will set ~6 bits in the
 // returned hash.
 uint64_t Globals::generate_hash(std::string data)
 {
