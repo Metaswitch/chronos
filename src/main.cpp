e3ddd9f4676e2887a9b031eaf1cd12dcfec7efc9
steelyvoid <sl@dataconnection.com>
2016-03-11 16:50:22 +0000

    Switch ENT logging channel to local7

--- src/main.cpp
+++ src/main.cpp
@@ -198,7 +198,7 @@ int main(int argc, char** argv)
   // Copy the filename to a string so that we can be sure of its lifespan -
   // the value passed to openlog must be valid for the duration of the program.
   std::string filename = p.filename().c_str();
-  openlog(filename.c_str(), PDLOG_PID, PDLOG_LOCAL6);
+  openlog(filename.c_str(), PDLOG_PID, PDLOG_LOCAL7);
   CL_CHRONOS_STARTED.log();
 
   // Log the PID, this is useful for debugging if monit restarts chronos.
